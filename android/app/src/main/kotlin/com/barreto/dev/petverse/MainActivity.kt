4f3eb1062e519a49fe50a097d97e421fd55bca7a
barretodevelop <barreto.develop@gmail.com>
2025-05-30 16:46:55 -0300

    Remove arquivos sensíveis e desnecessários do repositório

--- android/app/src/main/kotlin/com/barreto/dev/petverse/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.barreto.dev.petverse
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/feature/minigames/games/game.dart
+++
@@ -1,9 +0,0 @@
-// lib/features/home/home.dart
-
-// Exporta a página principal
-// export 'minigames/games/FoodCatcherGame.dart';
-
-// // Exporta todos os widgets importantes da home
-// export 'presentation/widgets/pet_card.dart';
-// export 'presentation/widgets/user_info_banner.dart';
-// export 'presentation/widgets/home_header.dart';
